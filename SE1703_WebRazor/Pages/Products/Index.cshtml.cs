da418927add0911a124511d5aab6bf71f4c82956
bachbackend <lebach09122002@gmail.com>
2024-02-19 14:43:41 +0700

    Add project files.

---
+++ SE1703_WebRazor/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SE1703_WebRazor.Models;
+
+namespace SE1703_WebRazor.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SE1703_WebRazor.Models.eStoreContext _context;
+
+        public IndexModel(SE1703_WebRazor.Models.eStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        [BindProperty]
+        public string id { get; set; }
+        public async Task OnGetAsync(int id)
+        {
+            if (_context.Products != null)
+            {
+                Product = await _context.Products
+                .Include(p => p.Category).ToListAsync();
+            }
+        }
+
+        
+    }
+}
